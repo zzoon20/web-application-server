0da49414fe03604af87b3360e36a34e2859f5de2
zzoon20 <zzoon20@211.241.237.68>
2016-07-05 14:58:36 +0900

    HttpResponse 단위테스트 통과

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -1,26 +1,138 @@
 package http;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import webserver.RequestHandler;
 
 public class HttpResponse {
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-	public HttpResponse(OutputStream createOutputStream) {
-		// TODO Auto-generated constructor stub
+	private DataOutputStream dos;
+	private Map<String, String> headers;
+
+	public HttpResponse(OutputStream out) {
+		dos = new DataOutputStream(out);
+		headers = new HashMap<String, String>();
 	}
 
-	public void sendRedirect(String string) {
-		// TODO Auto-generated method stub
-		
+	public void forward(String url) throws IOException {
+		responseResource(getDos(), url);
 	}
 
-	public void addHeader(String string, String string2) {
-		// TODO Auto-generated method stub
-		
+	public void sendRedirect(String url) {
+		response302Header(dos, url);
+		responseBody(dos, "".getBytes());
 	}
 
-	public void forward(String string) {
-		// TODO Auto-generated method stub
-		
+	public void addHeader(String key, String value) {
+		Map<String, String> tmp = getHeaders();
+		tmp.put(key, value);
+		setHeaders(tmp);
 	}
 
+	private void responseResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		response200Header(dos, body.length);
+		responseBody(dos, body);
+	}
+
+	private void responseCssResource(OutputStream out, String url) throws IOException {
+		DataOutputStream dos = new DataOutputStream(out);
+		byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+		response200CssHeader(dos, body.length);
+		responseBody(dos, body);
+	}
+
+	private int getContentLength(String line) {
+		String[] headerTokens = line.split(":");
+		return Integer.parseInt(headerTokens[1].trim());
+	}
+
+	private String getDefaultUrl(String[] tokens) {
+		String url = tokens[1];
+		if (url.equals("/")) {
+			url = "/index.html";
+		}
+		return url;
+	}
+
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void response302Header(DataOutputStream dos, String url) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+			dos.writeBytes("Location: " + url + " \r\n");
+			dos.writeBytes(getHeader());
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private String getHeader() {
+		StringBuilder sb = new StringBuilder();
+		Iterator<String> iterator = headers.keySet().iterator();
+		while(iterator.hasNext()){
+			String key = iterator.next();
+			sb.append(key + ": " + headers.get(key) + " \r\n");
+		}
+		return sb.toString();
+	}
+
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.writeBytes("\r\n");
+			dos.flush();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	public DataOutputStream getDos() {
+		return dos;
+	}
+
+	public void setDos(DataOutputStream dos) {
+		this.dos = dos;
+	}
+	
+	public Map<String, String> getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(Map<String, String> headers) {
+		this.headers = headers;
+	}
 }
