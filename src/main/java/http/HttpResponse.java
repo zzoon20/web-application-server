07f9b8caf8a16f705c0553a862b8131d5a759e5c
zzoon20 <zzoon20@211.241.237.68>
2016-07-05 14:17:42 +0900

    HttpResponse init

---
+++ src/main/java/http/HttpResponse.java
@@ -0,0 +1,21 @@
+package http;
+
+import java.io.OutputStream;
+
+public class HttpResponse {
+
+	public HttpResponse(OutputStream createOutputStream) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void sendRedirect(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addHeader(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
