20452e1ca9927249251988ecb75995b53be20ac3
zzoon20 <zzoon20@218.38.137.28>
2016-07-07 10:41:51 +0900

    getContentType 추가

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -48,14 +48,20 @@ public class HttpResponse {
 			log.debug("content-length = {}", lengthOfBodyContent);
 			
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: "+ (headers.get("Accept").split(", "))[0] + ";charset=utf-8\r\n");
+			dos.writeBytes("Content-Type: "+ getContentType() + ";charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
 	}
-
+	private String getContentType(){
+		String accept = headers.get("Accept");
+		if(accept == null) {
+			return "*/*";
+		}
+		return (accept.split(", "))[0];
+	}
 	private void responseBody(byte[] body) {
 		try {
 			dos.write(body, 0, body.length);
